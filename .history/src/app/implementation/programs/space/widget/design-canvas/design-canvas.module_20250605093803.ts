2c54b7a825f98656c89f36bb343551981c25640c
wuhuiying-3590 <wuhuiying3590@163.com>
2025-06-16 14:21:11 +0800

    delete .history

--- .history/src/app/implementation/programs/space/widget/design-canvas/design-canvas.module_20250605093803.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { ListItemComponent } from '../list-item/list-item.component';
-@NgModule({
-  declarations: [ListItemComponent],
-  imports: [CommonModule],
-})
-export class DesignCanvasModule {}
